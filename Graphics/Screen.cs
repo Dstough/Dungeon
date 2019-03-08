d0533a95a34d3372f5dd39638ee88cbc3187296d
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-03-08 13:45:28 -0600

    Got the screen to refresh to a blue color.

--- Graphics/Screen.cs
+++ Graphics/Screen.cs
@@ -8,39 +8,45 @@ public partial class Screen : Form
 {
     Size cellSize;
     Thread thread;
+    Bitmap image;
+    Font font;
     Graphics buffer;
     Graphics screen;
-    Bitmap image;
 
     public Screen()
     {
         InitializeComponent();
     }
+
     private void onLoad(object sender, EventArgs e)
     {
-        cellSize = new Size(this.ClientSize.Width / Config.screenWidth, this.ClientSize.Height / Config.roomHeight);
-        image = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
-        buffer = Graphics.FromImage(image);
-        screen = CreateGraphics();
+        OnResizeEnd(e);
         thread = new Thread(draw);
         thread.IsBackground = true;
         thread.Start();
     }
+
     protected override void OnResizeEnd(EventArgs e)
     {
-        image = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+        cellSize = new Size(ClientSize.Width / Config.screenWidth, ClientSize.Height / Config.roomHeight);
+        image = new Bitmap(ClientSize.Width, ClientSize.Height);
+        font = new Font("DungeonFont", cellSize.Width, FontStyle.Regular);
         buffer = Graphics.FromImage(image);
+        screen = CreateGraphics();
     }
+
     public void draw()
     {
         while (!Program.quit)
         {
-
+            buffer.Clear(Color.Blue);
+            drawRoom();
+            screen.DrawImage(image,0,0);
         }
     }
 
     void drawRoom()
     {
-
+        
     }
 }
