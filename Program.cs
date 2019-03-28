69418103a9e3f9ccc064469e8aadf4c6a4feffe5
Daniel <Daniel.Stoughton@gmail.com>
2019-03-27 20:20:51 -0500

    Moved the level generator to its own class

--- Program.cs
+++ Program.cs
@@ -17,32 +17,11 @@ static class Program
     {
         try
         {
-            for (int x = 0; x < Config.screenWidth; x++)
-            {
-                var topWall = new Wall();
-                var bottomWall = new Wall();
-                topWall.position = new Position(x, 0);
-                bottomWall.position = new Position(x, Config.roomHeight-1);
-                dungeon.levels[dungeon.currentLevel].objects.Add(topWall);
-                dungeon.levels[dungeon.currentLevel].objects.Add(bottomWall);
-            }
-
-            for(int y = 0; y < Config.roomHeight; y++)
-            {
-                var leftWall = new Wall();
-                var rightWall = new Wall();
-                leftWall.position = new Position(0, y);
-                rightWall.position = new Position(Config.screenWidth-1, y);
-                dungeon.levels[dungeon.currentLevel].objects.Add(leftWall);
-                dungeon.levels[dungeon.currentLevel].objects.Add(rightWall);
-            }
-
-
+            LevelBuilder.drawBorder();
             dungeon.levels[dungeon.currentLevel].actors.Add(hero);
 
             graphicsThread.IsBackground = true;
             graphicsThread.Start();
-
             while (!quit)
             {
                 foreach (var level in dungeon.levels)
@@ -51,7 +30,7 @@ static class Program
                     {
                         while (actor.initiative >= 12)
                         {
-                            Action action = new IdleAction();
+                            Action action;
                             do
                             {
                                 action = actor.takeTurn();
--- World/LevelBuilder.cs
+++ World/LevelBuilder.cs
@@ -1,7 +1,24 @@
-class LevelBuilder
+static class LevelBuilder
 {
-    public LevelBuilder()
+    public static void drawBorder()
     {
-        
+        for (int x = 0; x < Config.screenWidth; x++)
+        {
+            var topWall = new Wall();
+            var bottomWall = new Wall();
+            topWall.position = new Position(x, 0);
+            bottomWall.position = new Position(x, Config.roomHeight - 1);
+            Program.dungeon.levels[Program.dungeon.currentLevel].objects.Add(topWall);
+            Program.dungeon.levels[Program.dungeon.currentLevel].objects.Add(bottomWall);
+        }
+        for (int y = 0; y < Config.roomHeight; y++)
+        {
+            var leftWall = new Wall();
+            var rightWall = new Wall();
+            leftWall.position = new Position(0, y);
+            rightWall.position = new Position(Config.screenWidth - 1, y);
+            Program.dungeon.levels[Program.dungeon.currentLevel].objects.Add(leftWall);
+            Program.dungeon.levels[Program.dungeon.currentLevel].objects.Add(rightWall);
+        }
     }
 }
